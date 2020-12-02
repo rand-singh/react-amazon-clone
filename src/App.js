c411ff006c34423f034a7575908d07d411089ae7
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-02 14:48:22 +0000

    Setup react router and created checkout page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Checkout from './Checkout';
+
 
 function App() {
 	return (
 		<Router>
 			<div className="app">
+				<Header />
 				<Switch>
 					<Route path ="/checkout">
-						<Header />
-						<h1>basket</h1>
+						<Checkout />
 					</Route>
 
 					{/* default route at the bottom */}
 					<Route path ="/">
-						<Header />
 						<Home />
 					</Route>
 				</Switch>
--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Header.scss';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className="header">
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon Logo"/>
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon Logo"/>
+            </Link>
+
             <div className="header__search">
                 <input type="text" className="header__searchInput"/>
                 <SearchIcon className="header__searchIcon" />
@@ -25,10 +29,12 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>                    
                 </div>
 
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__optionLineTwo header__basketCount">0</span>
-                </div>
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
