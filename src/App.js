48ae0029424de912f678e61df15aa4eda75b47c2
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-02 12:30:44 +0000

    Setup header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="app">     
-      <h1>Amazon Clone</h1> 
+    <div className="app">
+      <Header />
+      <Home />
     </div>
   );
 }
