05982080bfdc881800e18791828d0829a2b63454
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-11 14:37:10 +0000

    Update Login.js
    Attached event handlers and state

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.scss'
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        // firebase login auth here
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        // firebase register here
+    }
+
     return (
         <div className="login">
             <Link to='/'>
@@ -13,17 +28,16 @@ function Login() {
                 <h1>Sign In</h1>
                 <form>
                     <h5>Email</h5>
-                    <input type="text"/>
-
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
                     
                     <h5>Password</h5>
-                    <input type="password"/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
 
-                    <button className="login__signInButton">Sign In</button>
+                    <button type="submit" onClick={signIn} className="login__signInButton">Sign In</button>
 
                     <p>By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &amp; Sale. Please see our Privacy Notic, our Cookies Notice and our Interest-Based Ads Notice</p>
 
-                    <button className="login__registerButton">Create an Amazon Account </button>
+                    <button onClick={register} className="login__registerButton">Create an Amazon Account </button>
                 </form>
             </div>
         </div>
