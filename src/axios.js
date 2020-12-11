d5bfd7cd8162023705d56e1855988f8cf90d0691
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-11 22:57:48 +0000

    Added prod url for cloud functions

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // THE API URL (cloud function)
-    baseURL: 'http://localhost:5001/challenge-45940/us-central1/api' 
+    baseURL: 'https://us-central1-challenge-45940.cloudfunctions.net/api'
+    // baseURL: 'http://localhost:5001/challenge-45940/us-central1/api' 
 });
 
 export default instance;
