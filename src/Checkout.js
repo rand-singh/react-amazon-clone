783525664a09534a7ce6502c013612cf573b22a7
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-11 16:00:24 +0000

    Display user email on basket and header

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 function Checkout() {
-    const [{basket}] = useStateValue();
+    const [{basket, user}] = useStateValue();
 
     return (
         <div className="checkout">
@@ -14,6 +14,7 @@ function Checkout() {
                 <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
 
                 <div>
+                    <h3>Hello {user?.email}</h3>
                     <h2 className="checkout__title">
                         Your shopping basket
                     </h2>
