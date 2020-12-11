3d46ef10622d8ce5efbba41181b536fc6a366b01
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-11 15:55:54 +0000

    Implemented user authentication using firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login'
-
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+	const [{}, dispatch] = useStateValue();
+
+	useEffect(() => {
+
+		auth.onAuthStateChanged(authUser => {
+			console.log('the user', {authUser});
+
+			if (authUser) {
+				// the just logged in or already logged in	
+				
+				dispatch({
+					type: 'SET_USER',
+					user: authUser
+				});				
+			} else {
+				// user is logged out
+				dispatch({
+					type: 'SET_USER',
+					user: null
+				});			}
+		})		
+		return () => {
+			// cleanup tasks here
+		}
+		// when we pass an empty array, this useEffect will on run once when app component loads
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
 	return (
 		<Router>
 			<div className="app">
